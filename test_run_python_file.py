1b04c66b63db2335e731507ef4240032a669cee6
thunt <thunt@example.com>
2026-03-07 08:04:59 -0500

    Update

---
+++ test_run_python_file.py
@@ -0,0 +1,12 @@
+from functions.run_python_file import run_python_file
+
+def main():
+  working_dir = "calculator"
+  print(run_python_file(working_dir, "main.py"))
+  print(run_python_file(working_dir, "tests.py"))
+  print(run_python_file(working_dir, "../main.py"))
+  print(run_python_file(working_dir, "nonexistent.py"))
+  print(run_python_file(working_dir, "lorem.txt"))
+  print(run_python_file(working_dir, "main.py", ["3 + 5"]))
+
+main()
